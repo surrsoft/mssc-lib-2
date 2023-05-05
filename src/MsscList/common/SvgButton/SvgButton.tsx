9cac100bb58327890c97bfb4391348d00480c1a1
surr <rybinsk.ru@gmail.com>
2023-05-05 22:43:24 +0300

    next

--- src/MsscList/common/SvgButton/SvgButton.tsx
+++ src/MsscList/common/SvgButton/SvgButton.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
-import { ReactComponent as IconEdit } from './IconEdit.svg';
+import { ReactComponent as SvgIconEdit } from './IconEdit.svg';
+import { SvgButtonBaseStyled } from './SvgButtonBaseStyled';
+
+const SvgButtonStyled = styled.button`
+  ${SvgButtonBaseStyled};
+
+  border: 1px silver solid;
+  border-radius: 8px;
+  background-color: transparent;
+  width: 42px;
+  height: 42px;
+`;
 
 export function SvgButton() {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const handleClick = () => {
+    setIsLoading(!isLoading);
+  };
+
   return <div>
     <div>SvgButton</div>
-    <IconEdit />
-  </div>
+    <SvgButtonStyled
+      disabled={isLoading}
+      colors={{ normal: 'blue', hover: 'red', disabled: 'silver' }}
+      svgSizesPx={32}
+      onClick={handleClick}
+      isLoading={isLoading}
+    >
+      <SvgIconEdit />
+    </SvgButtonStyled>
+    <div>{isLoading ? 'true' : 'false'}</div>
+  </div>;
 }
---
+++ src/MsscList/common/SvgButton/SvgButtonBaseStyled.ts
@@ -0,0 +1,77 @@
+import { CSSProperties, css, keyframes } from 'styled-components';
+
+/** цвет в формате принятом в JS и CSS, например: '#000000', 'red' */
+type ColorType = string;
+/** размер ширины и высоты в пикселях */
+type SizesPxType = number;
+
+interface ButtonColorsType {
+  normal?: ColorType;
+  hover?: ColorType;
+  disabled?: ColorType;
+  click?: ColorType;
+}
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`;
+
+export const SvgButtonBaseStyled = css<{
+  colors?: ButtonColorsType,
+  svgSizesPx?: SizesPxType,
+  css?: CSSProperties,
+  isLoading?: boolean
+}>`
+  border: 0;
+  border-radius: 0;
+  padding: 0;
+  cursor: pointer;
+  width: ${({ css }) => (css?.width ?? 'unset')};
+  height: ${({ css }) => (css?.height ?? 'unset')};
+
+  svg {
+    width: ${({ svgSizesPx }) => `${svgSizesPx ?? 42}px`};
+    height: ${({ svgSizesPx }) => `${svgSizesPx ?? 42}px`};
+    fill: ${({ colors }) => (colors?.normal ?? 'unset')};;
+  }
+
+  &:hover svg {
+    fill: ${({ colors }) => (colors?.hover ?? colors?.normal ?? 'unset')};
+  }
+
+  &:active svg {
+    fill: ${({ colors }) => (colors?.click ?? colors?.normal ?? 'unset')};
+  }
+
+  &:disabled svg {
+    fill: ${({ colors }) => (colors?.disabled ?? colors?.normal ?? 'unset')};
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+  }
+
+  svg {
+    animation: fade ${({ isLoading }) => isLoading ? '1.2s' : '0'} infinite;
+    animation-timing-function: ease;
+  }
+
+  @keyframes fade {
+    0% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+
+`;
